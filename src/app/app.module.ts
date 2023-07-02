1d0009e37846bb8f4c6fdb020e9c817873ad6b74
Andrey <anymacstore@Andrii-2.local>
2023-07-02 19:21:54 +0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { CoursesService } from './courses.service';
+import { CoursesComponent } from './courses.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    CourseComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }  
